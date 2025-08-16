6839122f2e3aeaf5cd59382e81530e861b762028
mndns232 <a23.rw5e@g.chuo-u.ac.jp>
2025-08-16 19:24:01 +0900

    pinクラスと、dammyデータつくったよ

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,20 @@
+// lib/data/dummy_data.dart などのファイルを作成して保存する
+final List<Map<String, dynamic>> dummyPins = [
+  {
+    'title': '半年後に自作アプリで1万円稼ぐ',
+    'description': 'FlutterとFirebaseで作ったアプリを公開して、広告収入か有料機能で収益を出す',
+    'category': '仕事',
+    'userId': 'user_a_id',
+    'xPosition': 150.5,
+    'yPosition': 280.0,
+  },
+  {
+    'title': '世界一周旅行',
+    'description': 'いつか必ず成し遂げたい夢',
+    'category': '趣味',
+    'userId': 'user_b_id',
+    'xPosition': 300.2,
+    'yPosition': 150.0,
+  },
+  // さらにダミーデータを追加
+];
